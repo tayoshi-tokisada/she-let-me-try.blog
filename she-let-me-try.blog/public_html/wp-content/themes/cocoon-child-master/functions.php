c422ee0fdaa53d334f54dffca6e896b971dcd7fc
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2026-04-10 08:50:10 +0900

    change カスタムテキスト

--- she-let-me-try.blog/public_html/wp-content/themes/cocoon-child-master/functions.php
+++ she-let-me-try.blog/public_html/wp-content/themes/cocoon-child-master/functions.php
@@ -5,3 +5,26 @@ if ( !defined( 'ABSPATH' ) ) exit;
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
+//カスタムテキストメニューの数を増やす
+add_filter('cocoon_custom_text_count', function ($count){
+  $count = 1;
+  return $count;
+});
+
+// エディターのカスタムテキスト名を変更する
+add_filter('cocoon_custom_text_titles', function($titles) {
+    // 'custom1' の部分を任意の名前に書き換えます
+    $titles['custom1'] = '灰色';
+    $titles['custom2'] = '灰太字';
+
+    return $titles;
+});
+
+// ブロックエディターに独自のスタイル（クラス）を追加する
+register_block_style(
+    'core/paragraph', // 段落に適用する場合
+    array(
+        'name'  => 'my-custom-design', // これがクラス名（my-custom-design）になる
+        'label' => '自分専用デザイン',   // エディター上の表示名
+    )
+);
